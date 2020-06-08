efc90a7fcf8594243035ef9c1caccf2ea16d7b15
Varun Jit Singh <varun@administrators-MacBook-Pro-2.local>
2020-06-08 11:59:41 -0700

    Add menu icon

--- app/src/main/java/com/codepath/apps/restclienttemplate/TimelineActivity.java
+++ app/src/main/java/com/codepath/apps/restclienttemplate/TimelineActivity.java
@@ -2,6 +2,9 @@ package com.codepath.apps.restclienttemplate;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
@@ -99,6 +102,24 @@ public class TimelineActivity extends AppCompatActivity {
                 Log.e(TAG, "on failure for on load more data");
             }
         }, tweets.get(tweets.size()-1).id);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        if (item.getItemId() == R.id.compose)
+        {
+            Toast.makeText(this, "Compose", Toast.LENGTH_SHORT).show();
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 
     private void populateHomeTimeline() {
